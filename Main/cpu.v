c40f2ddfedb356a3d70097a8f13e207a67c2d075
Nick Levin <itsmenicklevin@gmail.com>
2015-04-09 18:13:35 -0500

    update to cpu through ID module

--- Main/cpu.v
+++ Main/cpu.v
@@ -36,27 +36,28 @@ instr_fetch instr_fetch(clk, rst_n, IF_instr_mem_re, IF_PC, IF_instr);
 //
 
 //ID Stage Wire Declarations
-wire[21:0] ID_PC, ID_PC_out;
-wire[31:0] ID_instr, ID_s_data, ID_t_data; 
 wire ID_hlt, ID_use_imm, ID_use_dst_reg;
 wire ID_update_neg, ID_update_carry, ID_update_ov, ID_update_zero; //flag updates
 wire[2:0] ID_alu_opcode, ID_branch_conditions;
-wire[16:0] ID_imm; 
 wire[4:0] ID_dst_reg;
+wire[16:0] ID_imm; 
+wire[21:0] ID_PC, ID_PC_out;
+wire[31:0] ID_instr, ID_s_data, ID_t_data; 
+
 
 //sprite wires
-wire[7:0] ID_sprite_addr;
-wire[3:0] ID_sprite_action;
 wire ID_sprite_use_imm, ID_sprite_re, ID_sprite_we, ID_sprite_use_dst_reg;
+wire[3:0] ID_sprite_action;
+wire[7:0] ID_sprite_addr;
 wire[13:0] ID_sprite_imm;
 
 //SPART
-wire 
+wire IOR;
 
 //From WB Stage to Reg File in decode module
+wire WB_we;
 wire [4:0] WB_dst_reg_WB;
 wire [31:0] WB_dst_reg_data_WB;
-wire WB_we;
 
 
 //////////////////
@@ -69,10 +70,10 @@ wire WB_we;
 
 
 //ID Module Declaration
-instr_decode(clk,rst_n, instr, alu_opcode, imm, regS_data_ID, regT_data_ID, use_imm,
-	  use_dst_reg, branch_instr, update_neg, update_carry, update_overflow, update_zero, sprite_addr, 
-	  sprite_action, sprite_use_imm, sprite_imm, /*sprite_reg_data,*/ sprite_re, sprite_we, sprite_use_dst_reg, IOR, dst_reg, hlt,
-	  PC_in, PC_out, dst_reg_WB, dst_reg_data_WB, we);
+instr_decode(clk,rst_n, ID_instr, ID_alu_opcode, ID_imm, ID_s_data, ID_t_data, ID_use_imm,
+	  ID_use_dst_reg, ID_is_branch, ID_update_neg, ID_update_carry, ID_update_ov, ID_update_zero, ID_sprite_addr, 
+	  ID_sprite_action, ID_sprite_use_imm, ID_sprite_imm, /*sprite_reg_data,*/ ID_sprite_re, ID_sprite_we, ID_sprite_use_dst_reg, IOR, ID_dst_reg, ID_hlt,
+	  ID_PC, ID_PC_out, WB_dst_reg_WB, WB_dst_reg_data_WB, WB_we, ID_branch_addr, ID_branch_conditions);
 
 //////////////////
 //Execute
