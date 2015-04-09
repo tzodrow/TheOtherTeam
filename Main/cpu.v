b38a968d63d6ad9be030467330e2eb116140d17f
Nick Levin <itsmenicklevin@gmail.com>
2015-04-09 17:59:37 -0500

    Update cpu

--- Main/cpu.v
+++ Main/cpu.v
@@ -13,19 +13,51 @@ wire stall;
 
 //IF Stage Wire Declarations
 wire IF_instr_mem_re;
-wire[21:0] IF_pc, IF_next_pc;
+wire[21:0] IF_PC, IF_next_PC;
 wire[31:0] IF_instr;
+wire ID_is_branch; //used for determining next_PC value
+wire[21:0] ID_branch_addr; //used for determining 
 
 //IF Logic
-assign pc = IF_pc;
+assign pc = IF_PC;
 
-pc PC(clk, rst_n, IF_nxt_pc, IF_pc);
+pc PC(clk, rst_n, IF_next_PC, IF_PC);
 
 assign IF_instr_mem_re = ~stall; 	
-assign IF_nxt_pc = IF_pc + 1; //ADD logic for branching
+assign IF_next_PC = stall ? IF_PC : ID_is_branch ? ID_branch_addr : IF_PC + 1;
 
 //IF Module Declaration
-instr_fetch instr_fetch(clk, rst_n, IF_instr_mem_re, IF_pc, IF_instr);
+instr_fetch instr_fetch(clk, rst_n, IF_instr_mem_re, IF_PC, IF_instr);
+
+//
+//////////////////
+//Instruction Decode
+//////////////////
+//
+
+//ID Stage Wire Declarations
+wire[21:0] ID_PC, ID_PC_out;
+wire[31:0] ID_instr, ID_s_data, ID_t_data; 
+wire ID_hlt, ID_use_imm, ID_use_dst_reg;
+wire ID_update_neg, ID_update_carry, ID_update_ov, ID_update_zero; //flag updates
+wire[2:0] ID_alu_opcode, ID_branch_conditions;
+wire[16:0] ID_imm; 
+wire[4:0] ID_dst_reg;
+
+//sprite wires
+wire[7:0] ID_sprite_addr;
+wire[3:0] ID_sprite_action;
+wire ID_sprite_use_imm, ID_sprite_re, ID_sprite_we, ID_sprite_use_dst_reg;
+wire[13:0] ID_sprite_imm;
+
+//SPART
+wire 
+
+//From WB Stage to Reg File in decode module
+wire [4:0] WB_dst_reg_WB;
+wire [31:0] WB_dst_reg_data_WB;
+wire WB_we;
+
 
 //////////////////
 //PIPE: Instruction Fetch - Instruction Decode
@@ -33,28 +65,14 @@ instr_fetch instr_fetch(clk, rst_n, IF_instr_mem_re, IF_pc, IF_instr);
 
 
 
-
-
-//////////////////
-//Instruction Decode
-//////////////////
-
-//ID Stage Wire Declarations
-
-
 //ID Logic
 
 
 //ID Module Declaration
-
-
-//////////////////
-//PIPE: Instruction Decode - Execute
-//////////////////
-
-
-
-
+instr_decode(clk,rst_n, instr, alu_opcode, imm, regS_data_ID, regT_data_ID, use_imm,
+	  use_dst_reg, branch_instr, update_neg, update_carry, update_overflow, update_zero, sprite_addr, 
+	  sprite_action, sprite_use_imm, sprite_imm, /*sprite_reg_data,*/ sprite_re, sprite_we, sprite_use_dst_reg, IOR, dst_reg, hlt,
+	  PC_in, PC_out, dst_reg_WB, dst_reg_data_WB, we);
 
 //////////////////
 //Execute
@@ -62,6 +80,9 @@ instr_fetch instr_fetch(clk, rst_n, IF_instr_mem_re, IF_pc, IF_instr);
 
 //EX Stage Wire Declarations
 
+//////////////////
+//PIPE: Instruction Decode - Execute
+//////////////////
 
 //EX Logic
 
@@ -70,19 +91,14 @@ instr_fetch instr_fetch(clk, rst_n, IF_instr_mem_re, IF_pc, IF_instr);
 
 
 //////////////////
-//PIPE: Execute - Memory
-//////////////////
-
-
-
-
-
-//////////////////
 //Memory
 //////////////////
 
 //MEM Stage Wire Declarations
 
+//////////////////
+//PIPE: Execute - Memory
+//////////////////
 
 //MEM Logic
 
@@ -91,19 +107,14 @@ instr_fetch instr_fetch(clk, rst_n, IF_instr_mem_re, IF_pc, IF_instr);
 
 
 //////////////////
-//PIPE: Memory - Writeback
-//////////////////
-
-
-
-
-
-//////////////////
 //Writeback
 //////////////////
 
 //WB Stage Wire Declarations
 
+//////////////////
+//PIPE: Memory - Writeback
+//////////////////
 
 //WB Logic
 
