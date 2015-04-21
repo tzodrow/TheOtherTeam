4553bef7fc7d92887f6426a761116aa058277f8c
John L. Peterson <jackgopack4@gmail.com>
2015-04-21 16:25:50 -0500

    Sprite and map images

--- Main/sprite_addr.v
+++ Main/sprite_addr.v
@@ -6,13 +6,13 @@ module sprite_addr(address, coordinates, counter);
 	always@(*) begin
 		address = 0;
 		if(counter < 8) address = coordinates + counter;
-		else if(counter < 16) address = coordinates + 640 + counter - 8;
-		else if(counter < 24) address = coordinates + 1280 + counter - 16;
-		else if(counter < 32) address = coordinates + 1920 + counter - 24;
-		else if(counter < 40) address = coordinates + 2560 + counter - 32;
-		else if(counter < 48) address = coordinates + 3200 + counter - 40;
-		else if(counter < 56) address = coordinates + 3840 + counter - 48;
-		else 				  address = coordinates + 4480 + counter - 56;
+		else if(counter < 16) address = coordinates + 320*1 + counter - 8;
+		else if(counter < 24) address = coordinates + 320*2 + counter - 16;
+		else if(counter < 32) address = coordinates + 320*3 + counter - 24;
+		else if(counter < 40) address = coordinates + 320*4 + counter - 32;
+		else if(counter < 48) address = coordinates + 320*5 + counter - 40;
+		else if(counter < 56) address = coordinates + 320*6 + counter - 48;
+		else 				  address = coordinates + 320*7 + counter - 56;
 	end
 
 endmodule
