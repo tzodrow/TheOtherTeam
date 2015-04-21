ee8944c4b0d7ba1539ff3daf62fb2f60f227c796
John L. Peterson <jackgopack4@gmail.com>
2015-04-21 17:27:37 -0500

    streamlining pseudocode

--- resources/pseudocode.c
+++ resources/pseudocode.c
@@ -5,42 +5,45 @@ while(true) {
 		if(up) direction = up;
 		if(down) direction = down;
 	}
-	// check if running into wall
+	bool allowed_move = wall_check(direction, position);
+	if(!allowed_move) branch_to_keystroke;
+	draw_sprite;
+	// Check for interactions with other sprites
+	// update score/health as necessary
+	
+}
+
+bool wall_check(direction, position) {
 	if(direction == left) {
-		if(left_position - 1 == wall) {// memory read
-			// branch to next cycle, poll for keystroke
-		}
+		if(left_position - 1 == wall)
+			result = false;
 		else {
 			sprite_position_x -= 1;
-			draw_sprite;
+			result = true;
 		}
 	}
 	else if(direction == right) {
-		if(right_position + 1 == wall) {
-			// branch to next cycle, poll for keystroke
-		}
+		if(right_position + 1 == wall) 
+			result = false;
 		else {
 			sprite_position_x += 1;
-			draw_sprite;
+			result = true;
 		}
 	}
 	else if(direction == up) {
-		if(top_position + 1 == wall) {
-			// branch to next cycle, poll for keystroke
-		}
+		if(top_position + 1 == wall) 
+			result = false;
 		else {
 			sprite_position_y += 1;
-			draw_sprite;
+			result = true;
 		}
 	}
 	else if(direction == down) {
-		if(top_position - 1 == wall) {
-			// branch to next cycle, poll for keystroke
-		}
+		if(top_position - 1 == wall) 
+			result = false;
 		else {
 			sprite_position_y -= 1;
 			draw_sprite;
 		}
 	}
-
 }
