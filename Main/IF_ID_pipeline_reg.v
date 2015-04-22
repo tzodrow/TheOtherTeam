5d8b0f036bd54c1a9d009677ed07b41390e34424
AlexSeabloom <seabloom@wisc.edu>
2015-04-22 18:01:36 -0500

    Update IF_ID_pipeline_reg.v

--- Main/IF_ID_pipeline_reg.v
+++ Main/IF_ID_pipeline_reg.v
@@ -1,26 +1,26 @@
-module IF_ID_pipeline_reg(clk, rst_n, hlt, stall, flush, instr_IF, instr_ID, PC_IF, PC_ID);
+module IF_ID_pipeline_reg(clk, rst_n, hlt, stall, flush, IF_instr, ID_instr, IF_PC, ID_PC);
 
 input clk, rst_n, stall, flush, hlt;
-input [31:0]instr_IF;
-input [21:0]PC_IF;
+input [31:0]IF_instr;
+input [21:0]IF_PC;
 
-output reg [31:0]instr_ID;
-output reg [21:0]PC_ID;
-
-always @(posedge clk, negedge rst_n)
-	if (!rst_n)
-	  instr_ID <= 0;
-	else if (flush)
-	  instr_ID <= 0;
-	else if (!stall & !hlt)
-	  instr_ID <= instr_IF;
+output reg [31:0]ID_instr;
+output reg [21:0]ID_PC;
 
 always @(posedge clk, negedge rst_n)
 	if (!rst_n)
-	  PC_ID <= 0;
+	  ID_instr <= 0;
 	else if (flush)
-	  PC_ID <= 0;
+	  ID_instr <= 0;
 	else if (!stall & !hlt)
-	  PC_ID <= PC_IF;
+	  ID_instr <= IF_instr;
+
+always @(posedge clk, negedge rst_n)
+	if (!rst_n)
+	  ID_PC <= 0;
+	else if (flush)
+	  ID_PC <= 0;
+	else if (!stall & !hlt)
+	  ID_PC <= IF_PC;
 
 endmodule 
