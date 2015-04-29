851d880ff28d88b36badb8bddc13c426dec0e7b9
Tim Zodrow <tszodr@gmail.com>
2015-04-29 17:51:45 -0500

    Finished coe file that generates images for sprites.

--- Main/Python Scripts/image_gen.py
+++ Main/Python Scripts/image_gen.py
@@ -72,6 +72,166 @@ def draw_pacman_right(outfile):
             outfile.write(color['yellow'])
 
 
+# Draw the Left Facing PacMan
+def draw_pacman_left(outfile):
+    # First Line
+    for x in range(0, 8):
+        if x == 0 or x > 5:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Second Line
+    for x in range(0, 8):
+        if x < 2 or x == 7:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Third Line
+    for x in range(0, 8):
+        if x < 3:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Forth Line
+    for x in range(0, 8):
+        if x < 4:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Fifth Line
+    for x in range(0, 8):
+        if x < 4:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Sixth Line
+    for x in range(0, 8):
+        if x < 3:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Seventh Line
+    for x in range(0, 8):
+        if x < 2 or x == 7:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Eighth Line
+    for x in range(0, 8):
+        if x == 0 or x > 5:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+
+# Draw the Down Facing PacMan
+def draw_pacman_down(outfile):
+    # First Line
+    for x in range(0, 8):
+        if x < 2 or x > 5:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Second Line
+    for x in range(0, 8):
+        if x == 0 or x == 7:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Third Line
+    for x in range(0, 8):
+        outfile.write(color['yellow'])
+
+    # Forth Line
+    for x in range(0, 8):
+        outfile.write(color['yellow'])
+
+    # Fifth Line
+    for x in range(0, 8):
+        if x == 3 or x == 4:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Sixth Line
+    for x in range(0, 8):
+        if x > 1 or x < 6:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Seventh Line
+    for x in range(0, 8):
+        if x != 0 and x != 7:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Eighth Line
+    for x in range(0, 8):
+        outfile.write(color['black'])
+
+
+# Draw the Up Facing PacMan
+def draw_pacman_up(outfile):
+
+    # First Line
+    for x in range(0, 8):
+        outfile.write(color['black'])
+
+    # Second Line
+    for x in range(0, 8):
+        if x != 0 and x != 7:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Third Line
+    for x in range(0, 8):
+        if x > 1 or x < 6:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Forth Line
+    for x in range(0, 8):
+        if x == 3 or x == 4:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Fifth Line
+    for x in range(0, 8):
+        outfile.write(color['yellow'])
+
+    # Sixth Line
+    for x in range(0, 8):
+        outfile.write(color['yellow'])
+
+    # Seventh Line
+    for x in range(0, 8):
+        if x == 0 or x == 7:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+    # Eighth Line
+    for x in range(0, 8):
+        if x < 2 or x > 5:
+            outfile.write(color['black'])
+        else:
+            outfile.write(color['yellow'])
+
+
 def main(argv):
     print "Generating COE File..."
     o = open('image_rom.coe', 'w')
@@ -79,6 +239,9 @@ def main(argv):
     o.write("memory_initialization_radix=16;\n")
     o.write("memory_initialization_vector=\n")
 
+    draw_pacman_up(o)
+    draw_pacman_down(o)
+    draw_pacman_left(o)
     draw_pacman_right(o)
 
     print "Done!"
