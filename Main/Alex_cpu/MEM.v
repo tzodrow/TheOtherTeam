7f666f23cba58856e4f606f581ff956c479c8cfd
tzodrow <tszodr@gmail.com>
2015-05-05 22:52:35 -0500

    Extra wire

--- Main/Alex_cpu/MEM.v
+++ Main/Alex_cpu/MEM.v
@@ -33,7 +33,6 @@ module MEM(clk, rst_n, mem_data, addr, re, we, ALU_result, sprite_data, sprite_A
 	main_mem3 your_instance_name (
   .clka(clk), // input clka
   .rsta(~rst_n),
-  .ena((re | we)), // input ena
   .ena((re | we) | hlt), // input ena
   .wea(we), // input [0 : 0] wea
   .addra(mem_addr),//addr[4:0]), // input [4 : 0] addra
